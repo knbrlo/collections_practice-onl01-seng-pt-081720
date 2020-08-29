e7855687b9936fbd64a5d2a6f09bbc4039639548
Ken Barlow <kenbrlow@gmail.com>
2020-08-29 00:36:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,7 @@ def sort_array_char_count(array)
   #should return an array in ascending order sorted by the number of characters in the string (FAILED - 1)
   array.sort_by {|x| x.length}
 end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end
